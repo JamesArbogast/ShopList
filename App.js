aa440e4d43296afc3398d407ebad028a60327704
James Arbogast <jamesarbogast52@gmail.com>
2021-09-29 14:35:39 -0700

    text init

--- App.js
+++ App.js
@@ -1,15 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import {View, Text } from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <View><Text>Hello there, General Kenobi</Text></View>
-      <View><Text>Hello there, General Kenobi</Text></View>
-      <StatusBar style="auto" />
-    </View>
-  );
+const App = () => {
+  return(
+  <View>
+    <Text></Text>
+  </View>
+  )
 }
 
 const styles = StyleSheet.create({
